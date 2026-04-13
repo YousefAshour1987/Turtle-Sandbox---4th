0d016b5adff73bd84eccf6d40e7100135be73c0e
Your Name <ashouryousef96@gmail.com>
2026-04-13 16:27:09 -0500

    ITS NOT WHAT YOU THINK

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Turtle-Sandbox---4th
+Draw the symbol found on Japanese Temples.  
+In this assignment, you will build a simulation where turtles make the swastika. Note that:
+
+1. Swastika ≠ Hakenkreuz
+2. Swastika: It is an ancient symbol in the form of an equal-armed cross with each arm continued at a right angle, used especially in South Asia as a symbol of good luck or spirituality.
+3. Hakenkreuz: A symbol in the form of a black cross with each arm bent clockwise at a right angle, used as the emblem of the German Nazi party.
--- main.py
+++ main.py
@@ -6,11 +6,26 @@ window = Screen()
 window.bgcolor("teal")
 
 t = Turtle()
-t.setheading(180+45)
+# t.setheading(180+45)
+# t.speed(1)
+t.pensize(40)
 # code
-t.right(90)
-t.forward(100)
+for i in range(4):
+    t.left(90)
+    t.backward(100)
+    t.left(90)
+    t.backward(100)
+    t.penup()
+    t.left(90)
+    t.setposition(0,0)
+    t.pendown()
 t.penup()
-t.setheading(45)
+t.setposition(0, -200)
+t.pendown()
+t.color("black", "white")  # (outline, fill)
+# Start filling
+t.begin_fill()
+t.circle(200)  # Draw circle with given radius
+t.end_fill()
 
 window.mainloop()
