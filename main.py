171baa29d0daa00f41175597697fbb8362671dab
Your Name <ashouryousef96@gmail.com>
2026-04-13 15:59:01 -0500

    starting

--- main.py
+++ main.py
@@ -5,36 +5,12 @@ import random
 window = Screen()
 window.bgcolor("teal")
 
-turtle1 = Turtle()
-turtle1.shape("turtle")
-turtle1.color("white")
-turtle1.speed(255)
-# r = random.randint(0, 255)
-# g = random.randint(0, 255)
-# b = random.randint(0, 255)
-
-for step in range(100):
-    # r = random.randint(0, 255)
-    # g = random.randint(0, 255)
-    # b = random.randint(0, 255)
-    # turtle1.color(r,g,b)
-    turtle1.speed(255)
-    turtle1.forward(75)
-    turtle1.right(90)
-    turtle1.forward(75)
-    turtle1.right(90)
-    turtle1.forward(25)
-    turtle1.left(90)
-    turtle1.forward(25)
-    turtle1.backward(75)
-    turtle1.left(90)
-    turtle1.forward(25)
-    turtle1.left(90)
-    turtle1.forward(75)
-    turtle1.right(90)
-    
-
-
-
+t = Turtle()
+t.setheading(180+45)
+# code
+t.right(90)
+t.forward(100)
+t.penup()
+t.setheading(45)
 
 window.mainloop()
